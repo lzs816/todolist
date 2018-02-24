bad524f96da841b6a2212f5a1c5c89a0f8646a78
lizhongshuai.intern <lizhongshuai.intern@xiaoyezi.com>
2018-02-24 17:08:23 +0800

    todolist

---
+++ my-first-vue/src/components/componentA.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+      <h1>{{msg}}</h1>
+      <h1>{{msgfrom}}</h1>
+      <button @click="onClickMe">click!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+      return {
+          msg: 'hello from componentA'
+      }
+  },
+  props: ['msgfrom'],
+  methods:{
+      onClickMe: function(){
+          console.log(this.msgfrom)
+      }
+  }
+}
+</script>
+
+<style>
+    h1{
+        color:red;
+    }
+</style>
+
+
